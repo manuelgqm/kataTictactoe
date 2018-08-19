fe951406bfb852145ce589c0e807fcbe7af84141
Manuel García de Quirós Moreno <manuelgqm@gmail.com>
2018-08-19 10:10:04 +0200

    resolves game cases

--- test/KataSpec.js
+++ test/KataSpec.js
@@ -73,7 +73,7 @@ describe('A player', () => {
   })
 
   it('should start empty', () => {
-    expect(player.itemsCount()).to.be.eq(0);
+    expect(player.itemsCount).to.be.eq(0);
   });
 
   it('should add one item', () => {
@@ -99,11 +99,11 @@ describe('A player', () => {
 
 })
 
-xdescribe('A Tic Tac Toe Game', () => {
+describe('A Tic Tac Toe Game', () => {
   const Game = require('../lib/Game.js')
   const Player = require("../lib/Player");
 
-  it("should win the X", () => {
+  it("should end with player X as winner", () => {
     let playerX = new Player();
     let playerO = new Player();
     let game = new Game(playerX, playerO);
@@ -114,6 +114,41 @@ xdescribe('A Tic Tac Toe Game', () => {
     game.playerO.addItem("23");
     game.playerX.addItem("33");
 
-    expect(game.checkWinner()).to.eq("X");
+    expect(game.resolve()).to.eq("Game over. Result: player X wins the game!!");
+  });
+
+  it("should end with player O as winner", () => {
+    let playerX = new Player();
+    let playerO = new Player();
+    let game = new Game(playerX, playerO);
+
+    game.playerX.addItem("11");
+    game.playerO.addItem("31");
+    game.playerX.addItem("22");
+    game.playerO.addItem("32");
+    game.playerX.addItem("32");
+    game.playerO.addItem("33");
+
+    expect(game.resolve()).to.eq("Game over. Result: player O wins the game!!");
+  });
+
+  it("should end with Draw", () => {
+    let playerX = new Player();
+    let playerO = new Player();
+    let game = new Game(playerX, playerO);
+
+    game.playerX.addItem("11");
+    game.playerO.addItem("12");
+    game.playerX.addItem("13");
+
+    game.playerO.addItem("22");
+    game.playerX.addItem("21");
+    game.playerO.addItem("23");
+
+    game.playerX.addItem("32");
+    game.playerO.addItem("31");
+    game.playerX.addItem("33");
+
+    expect(game.resolve()).to.eq("Game ends in draw. Try again!");
   });
 });
