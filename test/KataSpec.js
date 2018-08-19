40210600166c9fb04f6fbf206f9856b2df83ca4a
Manuel García de Quirós Moreno <manuelgqm@gmail.com>
2018-08-19 09:31:41 +0200

    completes game rules

--- test/KataSpec.js
+++ test/KataSpec.js
@@ -6,17 +6,19 @@ const expect = chai.expect;
 
 chai.use(spies);
 
-describe("A Tic Tac Toe Game", () => {
+describe("A Game", () => {
   const Game = require('../lib/Game.js')
   let game
 
   beforeEach(() => {
-    let board = {};
-    game = new Game(board);
+    let playerX = { items: new Array(4).fill("")};
+    let playerO = { items: new Array(4).fill("")};
+    game = new Game(playerX, playerO);
   });
 
-  it("should have a board", () =>{
-    expect(game.board).to.be.ok;
+  it("should have cross and circle players", () =>{
+    expect(game.playerX).to.be.ok;
+    expect(game.playerO).to.be.ok;
   });
 
   it("should end when one player wins", () => {
@@ -24,54 +26,94 @@ describe("A Tic Tac Toe Game", () => {
     expect(game.checkWinner()).to.be.ok;
   });
 
-  it("should end when there is no room on the board", () =>{
-    let board = {};
-    chai.spy.on(board, "room", () => false);
-    game.board = board;
-    expect(game.board.room()).to.not.be.ok;
+  it("should end when there is no space left", () =>{
+    expect(game.hasSpace()).to.not.be.ok;
   });
+
+  it('should win if three in a row', () => {
+    let win_input_1 = "11;12;21;13"
+    let win_input_2 = "21;22;;31;32;23"
+    let win_input_3 = "31;32;33"
+    let input2 = "11;12;23"
+
+    expect(game.checkWinner(win_input_1)).to.be.ok;
+    expect(game.checkWinner(win_input_2)).to.be.ok;
+    expect(game.checkWinner(win_input_3)).to.be.ok;
+    expect(game.checkWinner(input2)).to.not.be.ok;
+  });
+
+  it('should return a winner if three in a colummn', () => {
+    let win_input_1 = "11;21;31;13"
+    let win_input_2 = "21;12;;32;22;23"
+    let win_input_3 = "13;23;33"
+    let input2 = "11;12;23"
+
+    expect(game.checkWinner(win_input_1)).to.be.ok;
+    expect(game.checkWinner(win_input_2)).to.be.ok;
+    expect(game.checkWinner(win_input_3)).to.be.ok;
+    expect(game.checkWinner(input2)).to.not.be.ok;
+  });
+
+  it('should return a winner when three in a diagonal', () => {
+    let win_input_1 = "11;22;33;13"
+    let win_input_2 = "13;22;;31;22;23"
+
+    expect(game.checkWinner(win_input_1)).to.be.ok;
+    expect(game.checkWinner(win_input_2)).to.be.ok;
+  })
 
 })
 
-describe('A board', () => {
-  const Board = require("../lib/Board");
-  let board
+describe('A player', () => {
+  const Player = require("../lib/Player");
+  let player
 
   beforeEach (() =>{
-    board = new Board();
+    player = new Player();
   })
 
   it('should start empty', () => {
-    expect(board.itemsCount()).to.be.eq(0);
+    expect(player.itemsCount()).to.be.eq(0);
   });
 
   it('should add one item', () => {
     let item = [];
-    board.addItem(item);
-    expect(board.containsItem(item)).to.be.ok;
+    player.addItem(item);
+    expect(player.containsItem(item)).to.be.ok;
   });
 
   it('should add two items', () => {
     let item1 = [];
     let item2 = [];
 
-    board.addItem(item1);
-    board.addItem(item2);
+    player.addItem(item1);
+    player.addItem(item2);
 
-    expect(board.containsItem(item1)).to.be.ok;
-    expect(board.containsItem(item2)).to.be.ok;
-  });
-
-  it("should throw a error if try to add more than 9 items", () => {
-    let items = new Array(9).fill([]);
-    let not_filling_item = [0,0];
-
-    board.items = items;
-    expect(board.addItem.bind(board, not_filling_item)).to.throw("Can't add item; not enought room in board");
+    expect(player.containsItem(item1)).to.be.ok;
+    expect(player.containsItem(item2)).to.be.ok;
   });
 
 });
 
-describe('A Item', () => {
+describe('A player', () => {
 
 })
+
+xdescribe('A Tic Tac Toe Game', () => {
+  const Game = require('../lib/Game.js')
+  const Player = require("../lib/Player");
+
+  it("should win the X", () => {
+    let playerX = new Player();
+    let playerO = new Player();
+    let game = new Game(playerX, playerO);
+
+    game.playerX.addItem("11");
+    game.playerO.addItem("12");
+    game.playerX.addItem("22");
+    game.playerO.addItem("23");
+    game.playerX.addItem("33");
+
+    expect(game.checkWinner()).to.eq("X");
+  });
+});
